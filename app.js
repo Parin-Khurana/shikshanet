98b4f531aeec4df836f038ee9aa88c7ca5f6d159
Rishik1507 <microsoftrishik@gmail.com>
2025-10-16 13:04:31 +0530

    class

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ app.set('view engine', 'ejs')
 app.get('/', (req, res) => {
     res.render('dash')
 })
+app.get('/class', (req, res) => {
+    res.render('class')
+})
 app.listen(3000, () => {
     console.log('Server is running on port 3000')   
 }  )
