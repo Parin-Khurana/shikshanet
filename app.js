5eec4b664da7813d2f2630417170f57070f5d893
Rishik1507 <microsoftrishik@gmail.com>
2025-10-19 18:32:50 +0530

    c

--- app.js
+++ app.js
@@ -138,7 +138,7 @@ app.post('/class', auth, async (req, res) => {
         return res.status(500).json({ error: "Error uploading or parsing file" });
     }
 });
-app.get('/test', (req, res) => {
+app.get('/test',auth,(req, res) => {
     res.render('test');
 });
 
